329f0307cb6b9d216b8cfa9f04cc1a9b1bd9e6a7
Roman Shulenkov <postmaster@mcu.by>
2020-03-01 12:34:45 +0300

    Update build_fpga.sh

--- scripts/build_fpga.sh
+++ scripts/build_fpga.sh
@@ -2,9 +2,6 @@ echo $(pwd)
 
 source /opt/Xilinx/Vivado/2018.3/settings64.sh
 
-mkdir example-prj
-setenv XILINX_TCLSTORE_USERAREA example-prj
-
 vivado -mode batch -source scripts/recreate_prj.tcl 
 vivado -mode batch -source scripts/build_bitstream.tcl 
 
